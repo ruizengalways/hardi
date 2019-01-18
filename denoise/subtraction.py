bd06628af01c7dd9b491d91fcc26c54b3484e8a7
Scott Trinkle <tscott.trinkle@gmail.com>
2019-01-18 14:36:35 -0600

    Adds split command for denoised volumes

--- denoise/subtraction.py
+++ denoise/subtraction.py
@@ -1,12 +1,14 @@
 import nibabel as nib
 
-data_img = nib.load('../x_dtifit/data.nii.gz')
+data_img = nib.load('../x_raw_data/raw_data.nii.gz')
 data = data_img.get_data()
 
 for k in [5, 7, 9]:
     print(k)
-    denoised_data = nib.load(
-        './denoised_k{}.nii.gz'.format(k)).get_data()
-    noise = data - denoised_data
-    nib.save(nib.Nifti1Image(noise, data_img.affine),
+    denoised_data_img = nib.load(
+        './denoised_k{}.nii.gz'.format(k))
+    denoised_data = denoised_data_img.get_data()
+    noise = data.astype(denoised_data.dtype) - denoised_data
+    nib.save(nib.Nifti1Image(noise, denoised_data_img.affine,
+                             denoised_data_img.header),
              'noise_k{}.nii.gz'.format(k))
