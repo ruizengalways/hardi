35ae142143708b781da5bafbe6ae61a72cbe0fe8
Scott Trinkle <tscott.trinkle@gmail.com>
2019-02-14 16:43:18 -0600

    Fixes bvecs file

--- preprocess/registered_full/make_new_bfiles.py
+++ preprocess/registered_full/make_new_bfiles.py
@@ -7,20 +7,18 @@ bvecfn = '../../x_dtifit/bvecs'
 gtab = gradient_table(bvalfn, bvecfn)
 
 bvals = 3000 * np.ones(160, dtype=np.int)
-bvals[::9] = 0
+bvals[::10] = 0
 
 bvecs = np.insert(gtab.bvecs[1:], np.arange(
     0, gtab.bvecs[1:].shape[0], 9), [0, 0, 0], axis=0)
 
 
-# with open('./bvals_raw', 'w') as f:
-#     for bval in bvals:
-#         f.write('{} '.format(bval))
+with open('./bvals_raw', 'w') as f:
+    for bval in bvals:
+        f.write('{} '.format(bval))
 
-# with open('./bvecs_raw', 'w') as f:
-#     for i in range(3):
-#         for val in bvecs[:, i]:
-#             f.write('{} '.format(val))
-#         f.write('\n')
-
-gtab_new = gradient_table('./bvals_raw', './bvecs_raw', atol=1)
+with open('./bvecs_raw', 'w') as f:
+    for i in range(3):
+        for val in bvecs[:, i]:
+            f.write('{} '.format(val))
+        f.write('\n')
--- visualize_gradients.py
+++ visualize_gradients.py
@@ -5,6 +5,7 @@ from dipy.viz import window, actor
 gtab = gradient_table('./x_dtifit/bvals', './x_dtifit/bvecs')
 
 ren = window.Renderer()
-ren.add(actor.point(gtab.bvecs[1:], window.colors.red, point_radius=0.01))
-ren.add(actor.point(-gtab.bvecs[1:], window.colors.green, point_radius=0.01))
+ren.add(actor.point(gtab.bvecs[1:], window.colors.yellow, point_radius=0.01))
+ren.add(actor.point(-gtab.bvecs[1:], window.colors.yellow, point_radius=0.01))
+ren.add(actor.axes(scale=(1, 1, 1)))
 window.show(ren)
